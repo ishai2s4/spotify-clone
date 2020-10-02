7b5269b72a85597da21a2b5496078cf608bdef48
ishai2s4 <ishai2s4@gmail.com>
2020-10-02 16:56:12 +0530

    Updates

---
+++ precache-manifest.a27f0170b6adc6a23c0ed7ed5f3c65f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dfe4e156f4f5e70d9c990072b91e70b",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "56dbcfdff9ef5c2cc215",
+    "url": "/spotify-clone/static/css/main.ed1ee8b9.chunk.css"
+  },
+  {
+    "revision": "5077609465c7006a6a0e",
+    "url": "/spotify-clone/static/js/2.91da812d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone/static/js/2.91da812d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56dbcfdff9ef5c2cc215",
+    "url": "/spotify-clone/static/js/main.e47ba741.chunk.js"
+  },
+  {
+    "revision": "52de21c6b549d6d1baa2",
+    "url": "/spotify-clone/static/js/runtime-main.f1339594.js"
+  }
+]);
